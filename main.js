381099683f52a1a071c38d1d56f165255153142c
Oskar Siewruk <os.siewruk@gmail.com>
2020-12-29 00:22:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const firstName = 'Oskar'
+const age = '23'
+alert('Uwaga');
+console.log('Wiadomość w konsoli');
+console.log(firstName);
+console.log(age)
+console.log(`Mam na imię ${firstName} oraz mam ${age} lata`)
