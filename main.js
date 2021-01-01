6d16a021542958cff95755db71bd43c3894afc97
Oskar Siewruk <os.siewruk@gmail.com>
2021-01-01 21:05:06 +0100

    box model - sizing, border, padding, margin

--- main.js
+++ main.js
@@ -5,3 +5,6 @@ console.log('Wiadomość w konsoli');
 console.log(firstName);
 console.log(age)
 console.log(`Mam na imię ${firstName} oraz mam ${age} lata`)
+
+console.log('Tutaj nauczysz się kilku nowych rzeczy')
+alert('Tutaj nauczysz się kilku nowych rzeczy')
